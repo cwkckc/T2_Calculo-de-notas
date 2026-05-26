3ba459171dd1d05d9a58b7b6c332c9d647f952cd
cwkckc <N00476685@upn.pe>
2026-05-26 09:28:25 -0500

    Programa de notas

--- Program.cs
+++ Program.cs
@@ -36,4 +36,87 @@ class Reporte
  
         return aprobados;
     }
+    //Programa principal
+    static void Main()
+    {
+        // 1.cantidad de notas
+        int cantidad = 0;
+        Console.Write("¿Cuántas notas deseas ingresar? ");
+ 
+        while (!int.TryParse(Console.ReadLine(), out cantidad) || cantidad <= 0)
+        {
+            Console.WriteLine("Error. el numero tiene que ser un numero entero.");
+            Console.Write("¿Cuantas notas deseas ingresar? ");
+        }
+ 
+        int[] notas = new int[cantidad];
+ 
+        Console.WriteLine();
+ 
+        // 2. Ingresar notas con validación
+        for (int i = 0; i < cantidad; i++)
+        {
+            int nota;
+            bool valida = false;
+ 
+            do
+            {
+                Console.Write($"Ingresa la nota {i + 1}: ");
+                bool esNumero = int.TryParse(Console.ReadLine(), out nota);
+ 
+                if (!esNumero || nota < 0 || nota > 20)
+                {
+                    Console.WriteLine("Nota invalida, ingresa entre 0 y 20.");
+                    valida = false;
+                }
+                else
+                {
+                    valida = true;
+                }
+            } while (!valida);
+ 
+            notas[i] = nota;
+            Console.WriteLine();
+        }
+ 
+        //3. Calcular estadísticas (por referencia)
+        double promedio = 0;
+        int maximo = 0, minimo = 0;
+ 
+        Estadisticas(notas, ref promedio, ref maximo, ref minimo);
+ 
+        //4. Contar aprobados (por valor)
+        int aprobados    = AprobadoNumeros(notas);
+        int desaprobados = cantidad - aprobados;
+ 
+        double pctAprobados    = (double)aprobados    / cantidad * 100;
+        double pctDesaprobados = (double)desaprobados / cantidad * 100;
+ 
+        //5. Imprimir reporte
+        Console.WriteLine("--- Reporte del Salón ---");
+
+        Console.Write("Notas Ingresadass: [");
+        for (int i = 0; i < notas.Length; i++)
+        {
+            Console.Write(notas[i]);
+            if (i < notas.Length -1)
+            Console.Write(", ");
+        }
+        Console.WriteLine("]");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- obj/Debug/net9.0/t2_calcularnotas.AssemblyInfo.cs
+++ obj/Debug/net9.0/t2_calcularnotas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("t2_calcularnotas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a68f6573264ec891683526961c792dad9a96368d")]
 [assembly: System.Reflection.AssemblyProductAttribute("t2_calcularnotas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("t2_calcularnotas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
